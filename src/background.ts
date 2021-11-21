d2cfeb97f860e3b8dd64e62139e19315f7ef7af7
Vince Broz <vince@broz.cc>
2021-11-21 13:23:45 -0500

    Update template

--- src/background.ts
+++ src/background.ts
@@ -2,16 +2,8 @@
 
 import * as _ from 'lodash';
 import { SuggestFunction } from './chrome-types';
-
-import {
-  actOnInputData, logError as logErrorOrig, logSuccess, pullOmniboxSuggestions,
-} from './opener_for_asana';
-
-// As of 4.4.4, TypeScript's control flow analysis is wonky with
-// narrowing and functions that return never.  This is a workaround:
-//
-// https://github.com/microsoft/TypeScript/issues/36753
-const logError: (err: string) => never = logErrorOrig;
+import { logError } from './error';
+import { actOnInputData, logSuccess, pullOmniboxSuggestions } from './opener_for_asana';
 
 const populateOmnibox = async (text: string, suggest: SuggestFunction) => {
   const suggestions = await pullOmniboxSuggestions(text);
